aa5261f0d53b8534554ad99cf54af04eb22b55a1
Kai Evans <hi@kaievans.co>
2023-01-16 13:43:08 +1100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<h1>Welcome to SvelteKit</h1>
+<p>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+<style global>
+    @tailwind utilities;
+    @tailwind components;
+    @tailwind base;
+</style>
