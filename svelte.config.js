7706058f8255919f8edf1e2be0ae913dd8c3845f
Kai Evans <hi@kaievans.co>
2023-01-16 21:55:27 +1100

    Adding some niceties

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,28 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 import sveltePreprocess from "svelte-preprocess";
+import autoprefixer from "autoprefixer";
 import { mdsvex } from "mdsvex";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
   preprocess: [
-    sveltePreprocess(),
+    sveltePreprocess({
+      postcss: {
+        plugins: [autoprefixer],
+      },
+    }),
     vitePreprocess(),
     mdsvex({
       extensions: [".md"],
+      rehypePlugins: [
+        rehypeSlug,
+        rehypeAutolinkHeadings,
+      ],
     }),
   ],
 
