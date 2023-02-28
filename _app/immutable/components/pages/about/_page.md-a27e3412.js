ece7fda40ed711d04dafd5fffb79ebf8738d77c6
Kai Evans <hi@kaievans.co>
2023-02-28 16:23:22 +1100

    Updates

---
+++ _app/immutable/components/pages/about/_page.md-a27e3412.js
@@ -0,0 +1,17 @@
+import{S as G,i as J,s as L,k as i,q as y,a as g,l as r,m as n,h as a,r as I,c as _,n as l,b as s,F as h,C as N}from"../../../chunks/index-920be987.js";function O(D){let o,u,v,H,k,b,M,E,d,f,w,S,A,c,W,x,p,q,P,m,C;return{c(){o=i("h1"),u=i("a"),v=i("span"),H=y("About"),k=g(),b=i("p"),M=y("Hi there, my name is Kai Evans, and I’m still figuring out what to write here."),E=g(),d=i("h2"),f=i("a"),w=i("span"),S=y("Random facts"),A=g(),c=i("p"),W=y(`At one point, I red roughly 300 books within a 5 years period. Mostly
+non-fiction, business, strategy, history, and such. My biggest year was 91
+books. Although, goodreads had counted a 5 volume edition of WWII history as one
+book that year, and I’m still a bit sensitive about it.`),x=g(),p=i("p"),q=y(`I learned how to surf by myself at the age 34 through books and youtube videos.
+I surfed non stop for almost 7 years after that. I’m still fascinated by the
+sport. The beach is my happy place.`),P=g(),m=i("p"),C=y(`I’m an amateur luthier. I’ve been around guitars since 8th grade, and I have
+build 9 of them so far. I started with electrics, but gradually shifted towards
+high end steel string acoustic ones. I can talk about guitars and woodworking
+for hours.`),this.h()},l(t){o=r(t,"H1",{id:!0});var e=n(o);u=r(e,"A",{"aria-hidden":!0,tabindex:!0,href:!0});var R=n(u);v=r(R,"SPAN",{class:!0}),n(v).forEach(a),R.forEach(a),H=I(e,"About"),e.forEach(a),k=_(t),b=r(t,"P",{});var T=n(b);M=I(T,"Hi there, my name is Kai Evans, and I’m still figuring out what to write here."),T.forEach(a),E=_(t),d=r(t,"H2",{id:!0});var K=n(d);f=r(K,"A",{"aria-hidden":!0,tabindex:!0,href:!0});var F=n(f);w=r(F,"SPAN",{class:!0}),n(w).forEach(a),F.forEach(a),S=I(K,"Random facts"),K.forEach(a),A=_(t),c=r(t,"P",{});var j=n(c);W=I(j,`At one point, I red roughly 300 books within a 5 years period. Mostly
+non-fiction, business, strategy, history, and such. My biggest year was 91
+books. Although, goodreads had counted a 5 volume edition of WWII history as one
+book that year, and I’m still a bit sensitive about it.`),j.forEach(a),x=_(t),p=r(t,"P",{});var z=n(p);q=I(z,`I learned how to surf by myself at the age 34 through books and youtube videos.
+I surfed non stop for almost 7 years after that. I’m still fascinated by the
+sport. The beach is my happy place.`),z.forEach(a),P=_(t),m=r(t,"P",{});var B=n(m);C=I(B,`I’m an amateur luthier. I’ve been around guitars since 8th grade, and I have
+build 9 of them so far. I started with electrics, but gradually shifted towards
+high end steel string acoustic ones. I can talk about guitars and woodworking
+for hours.`),B.forEach(a),this.h()},h(){l(v,"class","icon icon-link"),l(u,"aria-hidden","true"),l(u,"tabindex","-1"),l(u,"href","#about"),l(o,"id","about"),l(w,"class","icon icon-link"),l(f,"aria-hidden","true"),l(f,"tabindex","-1"),l(f,"href","#random-facts"),l(d,"id","random-facts")},m(t,e){s(t,o,e),h(o,u),h(u,v),h(o,H),s(t,k,e),s(t,b,e),h(b,M),s(t,E,e),s(t,d,e),h(d,f),h(f,w),h(d,S),s(t,A,e),s(t,c,e),h(c,W),s(t,x,e),s(t,p,e),h(p,q),s(t,P,e),s(t,m,e),h(m,C)},p:N,i:N,o:N,d(t){t&&a(o),t&&a(k),t&&a(b),t&&a(E),t&&a(d),t&&a(A),t&&a(c),t&&a(x),t&&a(p),t&&a(P),t&&a(m)}}}class U extends G{constructor(o){super(),J(this,o,null,O,L,{})}}export{U as default};
